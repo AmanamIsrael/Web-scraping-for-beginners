6b5605e917c4548810fcdbbaff4d3904e7c8d3f3
amanam israel <skaiflex6@gmail.com>
2021-09-27 01:36:22 +0100

    UPDATE: added comments and removed console.log's

--- index.js
+++ index.js
@@ -12,21 +12,23 @@ axios(url).then(response => {
     const htmlMarkup = response.data;
     const _parsedHTML = cheerio.load(htmlMarkup)
     const articles = []
-    console.log(_parsedHTML)
 
+    //extracting data you want from the website
     _parsedHTML('.article-list-item').each(function () {
        const title =  _parsedHTML(this).find('.article-list-desc').find('a').text().replace( /[\r\n]+/gm, "")
         const category = _parsedHTML(this).find('.article-list-pretitle').find('a').text()
         const url =  _parsedHTML(this).find('.article-list-desc').find('a').attr('href')
 
+        //appending data to existing
         articles.push({
             title,
             category,
             url
         })
     })
-    console.log(articles)
+    console.log(articles) //logging data to console
 }).catch(err => {
+    //catching errors
   console.log({
       error: err
   })
