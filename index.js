99d12617eb9d262a84e6a50adad0c3fdf05de78d
amanam israel <skaiflex6@gmail.com>
2021-09-27 01:32:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const PORT = 8000
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+//initialize express
+const app = express();
+
+//using axios to access the url and parse the response as HTML
+const url = 'https://techcabal.com/'
+axios(url).then(response => {
+    const htmlMarkup = response.data;
+    const _parsedHTML = cheerio.load(htmlMarkup)
+    const articles = []
+    console.log(_parsedHTML)
+
+    _parsedHTML('.article-list-item').each(function () {
+       const title =  _parsedHTML(this).find('.article-list-desc').find('a').text().replace( /[\r\n]+/gm, "")
+        const category = _parsedHTML(this).find('.article-list-pretitle').find('a').text()
+        const url =  _parsedHTML(this).find('.article-list-desc').find('a').attr('href')
+
+        articles.push({
+            title,
+            category,
+            url
+        })
+    })
+    console.log(articles)
+}).catch(err => {
+  console.log({
+      error: err
+  })
+})
+app.listen(PORT, ()=> console.log(`server is running at ${PORT}`))
+
